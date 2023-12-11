b1b2c931e8ace66e37e0f821346062b68664f595
JRDNVI <20096529.wit@gmail.com>
2023-12-11 15:36:38 +0000

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import moviesRouter from './api/movies';
 import usersRouter from './api/users';
+import authenticate from './authenticate';
 import './db';
 import defaultErrHandler from './errHandler'
 
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/movies',authenticate,  moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter);
 app.use(defaultErrHandler);
